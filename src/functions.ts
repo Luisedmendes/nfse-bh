75ed408d2ad12f9b8ca3993a01b22c84c24192f0
Luís Eduardo Mendes de Carvalho <luiseduardo@risestudio.com.br>
2024-05-14 15:37:04 -0300

    test

--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,9 @@
-export function sayHello({firstName, lastName}:
-   {firstName: string, lastName: string}) {
-    console.log(`Hello, ${firstName} ${lastName}!`);
-
+export function sayHello({
+  firstName,
+  lastName,
+}: {
+  firstName: string;
+  lastName: string;
+}) {
+  console.log(`Hello, ${firstName} ${lastName}!`);
 }
